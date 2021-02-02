5996e2cb99d5246b457f300866660090e8f381c9
StephSince93 <stephen@stemsoftware.com>
2021-02-02 17:32:39 -0600

    Added error handlers to async http calls, added functionality to server to fetch updated requests if down, cleaned up code

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,15 +5,34 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
 app.post('/events', (req,res) => {
     const event = req.body
 
-    axios.post('http://localhost:4000/events', event) // Posts
-    axios.post('http://localhost:4001/events', event) // Comments
-    axios.post('http://localhost:4002/events', event) // Query
-    axios.post('http://localhost:4003/events', event) // Moderation
+    events.push(event)
+
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log('Posts',err.message); // Posts
+    });
+
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log('Comments',err.message); // Comments
+    });
+
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log('Query',err.message); // Query
+    });
+
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log('Moderation',err.message); // Moderation
+    });
 
     res.send({status: 'OK'})
+})
+
+app.get('/events', (req,res) => {
+    res.send(events)
 })
 
 app.listen(4005, () => {
