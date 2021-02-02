9875f5d7c0c3a2b8a27397243f0940f880c5eacd
StephSince93 <stephen@stemsoftware.com>
2021-02-02 11:01:41 -0600

    Added event bus route events to other services

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -10,12 +11,12 @@ app.use(cors())
 const commentsByPostId = {}
 
 
-app.get('/posts/:id/comments', (req,res) => {
+app.get('/posts/:id/comments',(req,res) => {
 
     res.send(commentsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req,res) => {
+app.post('/posts/:id/comments', async (req,res) => {
     const commentId = randomBytes(4).toString('hex')
     const {content} = req.body
 
@@ -25,7 +26,22 @@ app.post('/posts/:id/comments', (req,res) => {
 
     commentsByPostId[req.params.id] = comments
 
+    await axios.post('http://localhost:4005/events',{
+        type:'CommentCreated',
+        data: {
+            id:commentId, 
+            content,
+            postid: req.params.id
+        }
+    })
+
     res.status(201).send(comments)
+})
+
+app.post('/events', (req,res) => {
+    console.log('recieved event', req.body.type)
+
+    res.send({})
 })
 
 app.listen(4001, ()=> {
