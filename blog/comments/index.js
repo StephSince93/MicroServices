9bd23f66e326d6e98cea117c545a3dd9a60a440d
StephSince93 <stephen@stemsoftware.com>
2021-02-02 16:11:09 -0600

    Added moderation service to handle comment statuses, linked it to other services

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -22,7 +22,7 @@ app.post('/posts/:id/comments', async (req,res) => {
 
     const comments = commentsByPostId[req.params.id] || []
 
-    comments.push({id:commentId, content})
+    comments.push({id:commentId, content, status: 'pending'})
 
     commentsByPostId[req.params.id] = comments
 
@@ -31,15 +31,35 @@ app.post('/posts/:id/comments', async (req,res) => {
         data: {
             id:commentId, 
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     })
 
     res.status(201).send(comments)
 })
 
-app.post('/events', (req,res) => {
+app.post('/events', async (req,res) => {
     console.log('recieved event', req.body.type)
+
+    const { type, data, status, content } = req.body
+    if (type === 'CommentModerated'){
+        const { postId, id, status} = data
+        const comments = commentsByPostId[postId]
+
+        const comment = post.comments.find(comment => comment.id === id)
+        comment.status = status
+        console.log(comment)
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
 
     res.send({})
 })
--- blog/query/index.js
+++ blog/query/index.js
@@ -23,11 +23,22 @@ app.post('/events', (req,res) => {
     }
 
     if(type === 'CommentCreated') {
-        console.log(data)
-        const { id, content, postId } = data
+
+        const { id, content, postId, status } = data
 
         const post = posts[postId]
-        post.comments.push({ id, content })
+        post.comments.push({ id, content, status })
+    }
+
+    if(type === 'CommentUpdated') {
+        console.log('here!')
+        const { id, content, postId, status } = data
+
+        const post = posts[postId]
+        const comment = post.comments.find(comment => comment.id === id)
+        console.log('Updated Comment!',comment)
+        comment.status = status
+        comment.content = content
     }
     console.log(posts)
     res.send({})
