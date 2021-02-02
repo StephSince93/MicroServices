762dc179ec48124866fe63b5ebd21204732cfa0b
StephSince93 <stephen@stemsoftware.com>
2021-02-02 08:53:04 -0600

    Added UI to show the posts created

--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -5,7 +5,7 @@ export default () => {
     const [title, setTitle] = useState('')
 
     const onSubmit = async (event) => {
-        event.preventdefault()
+        event.preventDefault()
 
         await axios.post('http://localhost:4000/posts',{
             title
